6817b2364600fb61a957247a368ca17752252f8a
shivasaicmi <shivasai.p@comakeit.com>
2023-02-23 16:11:30 +0530

    added get and post user routes

--- server.js
+++ server.js
@@ -3,7 +3,32 @@ import cors from "cors";
 
 const server = express();
 server.use(cors())
-server.use(express.json())
+server.use(express.json());
+
+
+const users = [
+
+    {
+        name:"shivasai",
+        age:10
+    },
+    {
+        name:"manikanta",
+        age:20
+    }
+];
+
+
+server.get("/users",(req,res)=>{
+    console.log("Get the users");
+    res.status(200).json(users);
+});
+
+server.post("/users",(req,res)=>{
+    console.log("Post to users");
+    users.push(req.body);
+    res.status(200).json(users);
+})
 
 server.post("/",(req,res)=>{
     console.log(req.body);
