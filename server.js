417b761cecfa219593e2f3fd9cef39b99e5ad536
ManikantaComakeit <manikanta.kaspa@comakeit.com>
2023-02-23 16:07:51 +0530

    products added

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const server = express();
 server.use(cors())
 server.use(express.json())
 
+let productList = [];
+
 server.post("/",(req,res)=>{
-    console.log(req.body);
-    //this is manikanta branch
-    //this is mani2 branch
-    res.status(200).json({});
+    const productName = req.body.name;
+  productList.push(productName);
+  res.status(201).send({ message: 'Product added successfully!' });
 })
 
 server.get("/",(req,res)=>{
     console.log("request recieved")
-    res.status(200).json({});
+    res.status(200).send(productList);
 })
 
 server.listen(3000,()=>{
