8b1820f8e4f52c96bbce31b148858eee06e7daf8
saipavanComakeit <saipavan.p@comakeit.com>
2023-02-23 16:37:34 +0530

    Added a user with id-2

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-import express from "express";
-import cors from "cors";
+const express=require("express");
+const cors=require("cors");
 
-const server = express();
-server.use(cors())
-server.use(express.json())
+const app=express();
+app.use(express.json());
+app.use(cors({origin:"*"}));
 
-server.post("/",(req,res)=>{
-    console.log(req.body);
-    res.status(200).json({});
+
+const history=[{"userId":"0",
+    "accessTime":"1:24 PM"
+},{"userId":"1","accessTime":"5:33 AM"},{"userId":"2","accessTime":"2:02 PM"}]
+
+//get all users
+app.get("/userHistory",(req,res)=>{
+    console.log("sdfs")
+    res.status(201).json({msg:history})
 })
 
-server.get("/",(req,res)=>{
-    console.log("request recieved")
-    res.status(200).json({});
+//specific user
+app.get("/userHistory/:id",(req,res)=>{
+    id=req.params.id
+    console.log("sdfs")
+    res.status(201).json({msg:history[id]})
 })
 
-server.listen(3000,()=>{
-    console.log("server is running")
+app.listen("3000",()=>{
+    console.log("server listening on port 3000");
 })
+
